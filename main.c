3fc31280c7ca325ad8a487533dead1a7d9853ce4
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2020-11-26 20:00:44 -0300

    se pá dá pra começa

--- main.c
+++ main.c
@@ -8,33 +8,33 @@
 
 int main(int argc, char* argv[]) {
     int i = 0;
-    char* v[5];
-    int pid[4];
-    int filhos = 0;
+    char v[5][50];
     while (1) {
         printf("acsh> ");
-        char tmp[50];
-        while (scanf("%s", tmp)) {
-            strcpy(v[i], tmp);
-            printf("%d %s \n", i, v[i++]);
-            // if (v[i] == "<3") {
-            //     pid[filhos] = fork();
-            //     printf("%d", pid[filhos]);
-            //     if (pid[filhos++] == 0) {
-            //         v[i + 1] = NULL;
-            //         execvp(v[0], v);
-            //     }
-            //     i = -1;
-            // }
-            // if (v[i] == "%") {
-            // }
-            // if (v[i] == "cd") {
-            // }
-            // if (v[i] == "exit") {
-            // }
-            // i++;
+        while (scanf("%s", v[i]) == 1) {
+            printf("%d %s \n", i, v[i]);
+            i++;
+            if (getchar() == '\n') {
+                break;
+            }
         }
-        printf("teste\n");
+        i = 0;
     }
     return 0;
 }
+// if (v[i] == "<3") {
+//     pid[filhos] = fork();
+//     printf("%d", pid[filhos]);
+//     if (pid[filhos++] == 0) {
+//         v[i + 1] = NULL;
+//         execvp(v[0], v);
+//     }
+//     i = -1;
+// }
+// if (v[i] == "%") {
+// }
+// if (v[i] == "cd") {
+// }
+// if (v[i] == "exit") {
+// }
+// i++;
